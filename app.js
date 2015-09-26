6a64759bacb1559b0a18d503ab76c34787c5e7f3
Annie <anniehoogendoorn@gmail.com>
2015-09-25 18:56:32 -0700

    Add app.js androutes.

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-var studentSignIn = angular.module('studentSignIn', []);
+var studentSignIn = angular.module('studentSignIn', ['ui.router']);
+
+studentSignIn.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+  url: "",
+  templateUrl: "partials/home.html"
+
+  $stateProvider.state('sign-in', {
+    url: "/sign-in",
+    templateUrl: "partials/sign-in.html",
+    controller: 'SignInCtrl'
+  });
+
+  $stateProvider.state('presence', {
+    url: "/presence",
+    templateUrl: "partials/presence.html",
+    controller: 'PresenceCtrl'
+  });
+
+});
