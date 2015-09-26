7604c343b87bd0e481229632c49578a204940fcd
Annie <anniehoogendoorn@gmail.com>
2015-09-25 19:25:58 -0700

    Add home.html page.

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ studentSignIn.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider.state('home', {
   url: "",
   templateUrl: "partials/home.html"
-
-  $stateProvider.state('sign-in', {
-    url: "/sign-in",
-    templateUrl: "partials/sign-in.html",
-    controller: 'SignInCtrl'
   });
 
-  $stateProvider.state('presence', {
-    url: "/presence",
-    templateUrl: "partials/presence.html",
-    controller: 'PresenceCtrl'
-  });
+  // $stateProvider.state('signIn', {
+  //   url: "/sign-in",
+  //   templateUrl: "partials/sign-in.html",
+  //   controller: 'SignInCtrl'
+  // });
+  //
+  // $stateProvider.state('presence', {
+  //   url: "/presence",
+  //   templateUrl: "partials/presence.html",
+  //   controller: 'PresenceCtrl'
+  // });
 
 });
---
+++ services/SignInFactory.js
@@ -0,0 +1,10 @@
+studentSignIn.factory('SignInFactory', function SignInFactory() {
+  var factory = {};
+  factory.students = [
+  { name: "Sam Schmidt" },
+  { name: "Jessica Martin" },
+  { name: "Sandy Smith" },
+  { name: "Ryan Samuels" },
+  { name: "Brentwood Davis" }
+  ];
+});
